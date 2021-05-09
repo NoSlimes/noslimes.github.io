f733b8d7186cd47855216620188e73907d56f4f7
NoSlimes <elias.wennerberg@gmail.com>
2021-05-09 14:35:43 +0200

    2

---
+++ RELA.js
@@ -0,0 +1,10 @@
+function func(){
+  console.log("img1 clicked!!!!!!!!!!!1");
+  var current = document.getElementById("img1").getAttribute("src"); 
+  document.getElementById("mainImg")
+
+}
+
+
+document.getElementById("img1").onclick = func;
+
---
+++ hej.js
@@ -0,0 +1,3 @@
+
+document.getElementById("button").style.background="red";
+
