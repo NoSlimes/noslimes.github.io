1acee5d9dd63b0359fbcc4b2e0251877c7688d37
NoSlimes <67907136+NoSlimes@users.noreply.github.com>
2023-03-10 14:45:31 +0100

    foot/nav

--- includeFile.js
+++ includeFile.js
@@ -1,9 +1,12 @@
 $(function() {
-    let filename = "index.html; // Default filename is index.html
-    let urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has("file")) {
-      filename = urlParams.get("file");
-    }
-    $("#file-placeholder").load(filename);
-  });
-  
+  let urlParams = new URLSearchParams(window.location.search);
+  let filename;
+  if (urlParams.has("file")) {
+    filename = urlParams.get("file");
+  } else {
+    filename = "default.html"; // Default filename if none is specified
+  }
+  $("#file-placeholder").load(filename);
+  $("#nav-placeholder").load("nav.html");
+  $("#foot-placeholder").load("foot.html");
+});
