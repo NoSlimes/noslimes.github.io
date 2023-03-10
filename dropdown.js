dfa92678ad0d5ed082297f39e73498ff53810087
NoSlimes <elias.wennerberg@gmail.com>
2023-03-10 23:16:02 +0100

    d

--- dropdown.js
+++ dropdown.js
@@ -1,13 +1,16 @@
-/* Get the dropdown button and dropdown content */
-var dropdownButton = document.getElementById("dropdown-btn");
-var dropdownContent = document.getElementById("dropdown-content");
-
-/* Add a click event listener to the dropdown button */
-dropdownButton.addEventListener("click", function() {
-  /* Toggle the display of the dropdown content block */
-  if (dropdownContent.style.display === "block") {
-    dropdownContent.style.display = "none";
-  } else {
-    dropdownContent.style.display = "block";
-  }
-});
+document.addEventListener("DOMContentLoaded", function() {
+    /* Get the dropdown button and dropdown content */
+    var dropdownButton = document.getElementById("dropdown-btn");
+    var dropdownContent = document.getElementById("dropdown-content");
+  
+    /* Add a click event listener to the dropdown button */
+    dropdownButton.addEventListener("click", function() {
+      /* Toggle the display of the dropdown content block */
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  });
+  
