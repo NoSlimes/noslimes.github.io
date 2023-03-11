78c60bf2b4ec69b06d7f25b934f94e66a1828b6e
NoSlimes <67907136+NoSlimes@users.noreply.github.com>
2023-03-11 13:57:26 +0100

    updated dropdowns

--- dropdown.js
+++ dropdown.js
@@ -1,32 +1,31 @@
 window.onload = function () {
-  /* Get the dropdown button and dropdown content */
-  var dropdownButton = document.getElementById("dropdown-btn");
-  var dropdownContent = document.getElementById("dropdown-content");
+  /* Get all dropdown buttons and dropdown contents */
+  var dropdownButtons = document.querySelectorAll(".dropbtn");
+  var dropdownContents = document.querySelectorAll(".dropdown-content");
 
-  /* Add a click event listener to the dropdown button */
-  dropdownButton.addEventListener("click", function () {
-    /* Toggle the display of the dropdown content block */
-    if (dropdownContent.style.display === "block") {
+  /* Add click event listeners to all dropdown buttons */
+  dropdownButtons.forEach(function (dropdownButton) {
+    dropdownButton.addEventListener("click", function (event) {
+      /* Prevent the click event from bubbling up to the body */
+      event.stopPropagation();
+
+      /* Get the corresponding dropdown content */
+      var dropdownContent = dropdownButton.nextElementSibling;
+
+      /* Toggle the display of the dropdown content block */
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  });
+
+  /* Add click event listener to body */
+  document.body.addEventListener("click", function () {
+    /* Hide all dropdown contents */
+    dropdownContents.forEach(function (dropdownContent) {
       dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
+    });
   });
-
-  /* Get the dropdown button and dropdown content */
-  var dropdownButton1 = document.getElementById("dropdown-btn-1");
-  var dropdownContent1 = document.getElementById("dropdown-content-1");
-
-  /* Add a click event listener to the dropdown button */
-  dropdownButton1.addEventListener("click", function () {
-    /* Toggle the display of the dropdown content block */
-    if (dropdownContent1.style.display === "block") {
-      dropdownContent1.style.display = "none";
-    } else {
-      dropdownContent1.style.display = "block";
-    }
-  });
-
-
-}
-
+};
