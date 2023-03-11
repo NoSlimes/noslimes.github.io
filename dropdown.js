a759cf4446529e015bdab7de74d47841bd83a043
NoSlimes <67907136+NoSlimes@users.noreply.github.com>
2023-03-11 12:52:35 +0100

    about me

--- dropdown.js
+++ dropdown.js
@@ -1,16 +1,32 @@
-window.onload = function() {
-    /* Get the dropdown button and dropdown content */
-    var dropdownButton = document.getElementById("dropdown-btn");
-    var dropdownContent = document.getElementById("dropdown-content");
-  
-    /* Add a click event listener to the dropdown button */
-    dropdownButton.addEventListener("click", function() {
-      /* Toggle the display of the dropdown content block */
-      if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-      } else {
-        dropdownContent.style.display = "block";
-      }
-    });
-  }
-  
+window.onload = function () {
+  /* Get the dropdown button and dropdown content */
+  var dropdownButton = document.getElementById("dropdown-btn");
+  var dropdownContent = document.getElementById("dropdown-content");
+
+  /* Add a click event listener to the dropdown button */
+  dropdownButton.addEventListener("click", function () {
+    /* Toggle the display of the dropdown content block */
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+
+  /* Get the dropdown button and dropdown content */
+  var dropdownButton = document.getElementById("dropdown-btn-1");
+  var dropdownContent = document.getElementById("dropdown-content-1");
+
+  /* Add a click event listener to the dropdown button */
+  dropdownButton.addEventListener("click", function () {
+    /* Toggle the display of the dropdown content block */
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+
+
+}
+
