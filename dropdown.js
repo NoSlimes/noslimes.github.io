c723af6b1e982c160ed31f35a396920862c28655
NoSlimes <67907136+NoSlimes@users.noreply.github.com>
2023-03-11 13:49:40 +0100

    dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -14,16 +14,16 @@ window.onload = function () {
   });
 
   /* Get the dropdown button and dropdown content */
-  var dropdownButton = document.getElementById("dropdown-btn-1");
-  var dropdownContent = document.getElementById("dropdown-content-1");
+  var dropdownButton1 = document.getElementById("dropdown-btn-1");
+  var dropdownContent1 = document.getElementById("dropdown-content-1");
 
   /* Add a click event listener to the dropdown button */
-  dropdownButton.addEventListener("click", function () {
+  dropdownButton1.addEventListener("click", function () {
     /* Toggle the display of the dropdown content block */
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
+    if (dropdownContent1.style.display === "block") {
+      dropdownContent1.style.display = "none";
     } else {
-      dropdownContent.style.display = "block";
+      dropdownContent1.style.display = "block";
     }
   });
 
