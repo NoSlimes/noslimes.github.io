e9ca10da4477a13fc92e28c7b7d6051121fc4f86
NoSlimes <67907136+NoSlimes@users.noreply.github.com>
2023-03-11 14:03:55 +0100

    test

--- dropdown.js
+++ dropdown.js
@@ -1,26 +1,27 @@
-window.onload = function() {
-  /* Get all dropdown buttons and dropdown content */
+window.onload = function () {
+  /* Get all dropdown buttons and dropdown contents */
   var dropdownButtons = document.querySelectorAll(".dropbtn");
   var dropdownContents = document.querySelectorAll(".dropdown-content");
 
-  /* Add click event listeners to all dropdown buttons */
-  dropdownButtons.forEach(function(button, index) {
-    button.addEventListener("click", function() {
-      /* Toggle the display of the corresponding dropdown content block */
-      if (dropdownContents[index].style.display === "block") {
-        dropdownContents[index].style.display = "none";
+  /* Add a click event listener to each dropdown button */
+  dropdownButtons.forEach(function (dropdownButton) {
+    dropdownButton.addEventListener("click", function () {
+      /* Toggle the display of the associated dropdown content block */
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
       } else {
-        dropdownContents[index].style.display = "block";
+        dropdownContent.style.display = "block";
       }
     });
   });
 
-  /* Add click event listener to the document to close dropdowns when clicking outside */
-  document.addEventListener("click", function(event) {
-    dropdownContents.forEach(function(content) {
-      if (event.target !== content && event.target.parentNode !== content.previousElementSibling) {
-        content.style.display = "none";
+  /* Add a click event listener to the document body to close dropdowns when clicked elsewhere */
+  document.body.addEventListener("click", function (event) {
+    dropdownContents.forEach(function (dropdownContent) {
+      if (event.target !== dropdownContent.previousElementSibling && dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
       }
     });
   });
-};
+}
